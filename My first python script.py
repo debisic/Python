17d060d3fbf8e54bd053392614e28dba92a87da3
debisic <coadebisi@gmail.com>
2022-09-15 13:20:04 +0200

    pyhton scripts

---
+++ My first python script.py
@@ -0,0 +1,30 @@
+from winreg import HKEY_LOCAL_MACHINE
+
+
+print("hello world")
+print("I am going to be succesful in this transition")
+A = 30; B = 20
+print (A+B)
+
+
+mynumber = 10
+mytext = "hello"
+print (mynumber)
+print (mytext)
+value = "10.2"
+number = float(value)
+print(number)
+number=str(value)
+print(number)
+A=3**2
+print(A)
+A= -6
+B=A/2
+C=A%2
+print(A)
+print(B)
+print (C)
+print(A%4)
+
+
+
